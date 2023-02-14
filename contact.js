693e7b3f9befbc34cadb16f3c9e28c9fa83e9b65
Johnny Chen <Johnnychen384@gmail.com>
2023-02-14 17:54:25 -0500

    finished mobile version of contact page w/ js added

---
+++ contact.js
@@ -0,0 +1,10 @@
+const names = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const sendBtn = document.getElementById("send");
+
+
+sendBtn.addEventListener("click", () => {
+    alert("Email Successfully Sent!");
+})
