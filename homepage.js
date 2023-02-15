885b3e512379750428dfc8ae82463ede67668b61
Johnny Chen <Johnnychen384@gmail.com>
2023-02-14 19:35:32 -0500

    added typewriter function to homepage

---
+++ homepage.js
@@ -0,0 +1,15 @@
+const homepageName = document.getElementById("homepageText")
+const txt = "I'm Johnny Chen"
+let i = 0
+
+const typeWriter = () => {
+    if(i < txt.length) {
+        homepageName.innerHTML += txt.charAt(i);
+        i += 1
+        setTimeout(typeWriter, 100)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(typeWriter, 2000);
+})
